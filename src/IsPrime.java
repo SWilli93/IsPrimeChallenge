e5bd4ae5c5c295d140345d92150433e24baaa075
Scott <ScottWilliamsSolutions@gmail.com>
2023-09-24 19:37:26 -0400

    New Class > IsPrime > new method isPrime > structure built

--- src/IsPrime.java
+++ src/IsPrime.java
@@ -1,3 +1,12 @@
 public class IsPrime {
+    public static void main(String[] args) {
+        System.out.println("0 is " +  (isPrime(0) ? "" : "Not ") + "a prime number");
+        System.out.println("1 is " +  (isPrime(1) ? "" : "Not ") + "a prime number");
+        System.out.println("2 is " +  (isPrime(2) ? "" : "Not ") + "a prime number");
+        System.out.println("3 is " +  (isPrime(3) ? "" : "Not ") + "a prime number");
+    }
 
+    public static boolean isPrime(int wholeNumber) {
+        return true;
+    }
 }
