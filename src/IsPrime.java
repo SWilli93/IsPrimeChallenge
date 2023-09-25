e8cf83db1367209c0b9e9530c7098b9bd9c9756a
Scott <ScottWilliamsSolutions@gmail.com>
2023-09-24 20:14:03 -0400

    added another for loop to find 3 prime numbers

--- src/IsPrime.java
+++ src/IsPrime.java
@@ -6,18 +6,35 @@ public class IsPrime {
         System.out.println("3 is " +  (isPrime(3) ? "" : "Not ") + "a prime number");
         System.out.println("8 is " +  (isPrime(8) ? "" : "Not ") + "a prime number");
         System.out.println("17 is " +  (isPrime(17) ? "" : "Not ") + "a prime number");
+
+        int pNumFound = 1;
+
+        for (int pnum = 1; pnum <= 1000; pnum++) {
+            if (isPrime(pnum)) {
+                System.out.println("true");
+                pNumFound++;
+                if (pNumFound == 3) {
+                    System.out.println("Found 3");
+                    break;
+                }
+            }
+        }
+
     }
 
     public static boolean isPrime(int wholeNumber) {
+
         if (wholeNumber <= 2) {
             return (wholeNumber == 2);
         }
 
-        for (int divisor = 2; divisor < wholeNumber; divisor++) {
+        for (int divisor = 2; divisor < wholeNumber / 2; divisor++) {
             if (wholeNumber % divisor == 0) {
                 return false;
             }
+
         }
+
         return true;
     }
 }
